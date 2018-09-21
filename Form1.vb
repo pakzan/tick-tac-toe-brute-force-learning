814c79c3324a0a1251edd8e983ccbbf5f6c0f6ee
pakzan <pakzzzan@hotmail.com>
2018-09-22 00:19:55 +0800

    significant update to machine learning

--- Form1.vb
+++ Form1.vb
@@ -7,10 +7,13 @@ Public Class Form1
 
     Dim box() As Integer = {0, 0, 0, 0, 0, 0, 0, 0, 0}
     Public Shared Turn As Integer
-    Dim statistic As New List(Of List(Of String))
+    Dim statistics As New Dictionary(Of String, Short)
+    Dim stat_key As String
     Dim movement As String
     Dim path As String = "c:\temp\MyTest.txt"
     Dim found As Boolean
+    Dim deep As Short = 3
+    Dim branch As Short = 3
 
     Private Sub Form1_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
 
@@ -22,11 +25,14 @@ Public Class Form1
     End Sub
 
     Private Sub PlayerTurn(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click, Label2.Click, Label3.Click, Label4.Click, Label5.Click, Label6.Click, Label7.Click, Label8.Click, Label9.Click
+        'cast Sender Object into Label
         Dim clickedLabel = TryCast(sender, Label)
 
+        'check if the label is empty
         If clickedLabel IsNot Nothing Then
             If clickedLabel.Text = "" Then
                 clickedLabel.Text = "X"
+                'switch to computer turn if player havent win
                 If Not CheckWin() Then
                     ComputerTurn()
                 End If
@@ -41,14 +47,17 @@ Public Class Form1
     End Sub
 
     Private Sub RecordMovement(ByVal sender As Object, ByVal e As System.EventArgs) Handles Label1.TextChanged, Label2.TextChanged, Label3.TextChanged, Label4.TextChanged, Label5.TextChanged, Label6.TextChanged, Label7.TextChanged, Label8.TextChanged, Label9.TextChanged
+        'cast Sender Object into Label
         Dim ChangedLabel = TryCast(sender, Label)
 
+
         If ChangedLabel IsNot Nothing Then
-            movement += Replace(ChangedLabel.Name, "Label", "")
+            Dim label_no = Replace(ChangedLabel.Name, "Label", "")
+            movement += label_no
             If ChangedLabel.Text = "X" Then
-                box(Replace(ChangedLabel.Name, "Label", "") - 1) = 1
+                box(label_no - 1) = 1
             ElseIf ChangedLabel.Text = "O" Then
-                box(Replace(ChangedLabel.Name, "Label", "") - 1) = 2
+                box(label_no - 1) = 2
             End If
         End If
     End Sub
@@ -56,33 +65,35 @@ Public Class Form1
     Public Sub ComputerTurn()
         Dim compLabel As Label
         Dim potential As Integer
-        Dim mark As Integer = 0
+        Dim reward As Integer = 0
         found = False
 
-        For i = 1 To statistic(0).Count - 1
-            If Integer.Parse(statistic(0)(i)(0)) = Turn And statistic(0)(i).Length - 1 > movement.Length And statistic(0)(i).Contains(movement) And mark <= Integer.Parse(statistic(1)(i)) Then
-                mark = Integer.Parse(statistic(1)(i))
-                potential = Integer.Parse(statistic(0)(i)(movement.Length + 1))
+        'check database to find similar record, follow the record with best result
+        'found: check if found good result(reward > 0)
+        'reward: result of previous record
+        'potential: movement of previous record
+        For Each statistic In statistics
+            If Integer.Parse(statistic.Key(0)) = Turn And statistic.Key.Length - 1 > movement.Length And
+                statistic.Key.StartsWith(movement) And reward <= statistic.Value Then
+                stat_key = statistic.Key
+                reward = statistic.Value
+                potential = Integer.Parse(statistic.Key(movement.Length + 1))
                 found = True
             End If
         Next
 
-        If found Then
-            compLabel = Me.Controls("Label" & potential)
-        Else
-            Dim number As Integer = RND.Next(1, 9)
-            For i = 0 To 8
-                number = ((number + 1) Mod 9) + 1
-                compLabel = Me.Controls("Label" & number)
-                If compLabel.Text = "" Then
-                    GoTo end_of_for
-                End If
-            Next
+        'prevent the comp from following previous record
+        Dim follow_record As Integer = RND.Next(0, 10)
+        If Not found Or follow_record < 7 Then
+            stat_key = ""
+            potential = Minimax(box, branch, deep, False) + 1
         End If
-end_of_for:
 
+        compLabel = Me.Controls("Label" & potential)
         compLabel.Text = "O"
         CheckWin()
+
+        'Reinforcement Learning
         'If Not CheckWin() Then
         '    ComputerTurn1()
         'End If
@@ -90,89 +101,166 @@ end_of_for:
 
     Public Sub ComputerTurn1()
         Dim compLabel As Label
-        Dim found As Boolean = False
+        Dim potential As Integer
+        Dim reward As Integer = 0
+        found = False
 
-'        For i = 1 To statistic.Length - 1
-'            If statistic(i)(0) = Convert.ToString(Turn) Then
-'                If statistic(i).Length - 1 < movement.Length Then
-'                    GoTo end_of_for
-'                End If
-'                For j = 0 To movement.Length - 1
-'                    If statistic(i)(j + 1) <> movement(j) Then
-'                        GoTo end_of_for
-'                    End If
-'                Next
-'                compLabel = Me.Controls("Label" & statistic(i)(movement.Length + 1))
-'                found = True
-'            End If
-'end_of_for:
-'        Next
+        'check database to find similar record, follow the record with best result
+        'found: check if found good result(reward > 0)
+        'reward: result of previous record
+        'potential: movement of previous record
+        For Each statistic In statistics
+            If Integer.Parse(statistic.Key(0)) = Turn And statistic.Key.Length - 1 > movement.Length And
+                statistic.Key.StartsWith(movement) And reward <= statistic.Value Then
+                stat_key = statistic.Key
+                reward = statistic.Value
+                potential = Integer.Parse(statistic.Key(movement.Length + 1))
+                found = True
+            End If
+        Next
 
-        If Not found Then
-            Dim number As Integer = RND.Next(1, 9)
-            Do
-                number = ((number + 1) Mod 9) + 1
-                compLabel = Me.Controls("Label" & number)
-            Loop Until compLabel.Text = ""
+        'prevent the comp from following previous record
+        Dim follow_record As Integer = RND.Next(0, 10)
+        If Not found Or follow_record < 7 Then
+            stat_key = ""
+            potential = Minimax(box, branch, deep, True) + 1
         End If
 
+        compLabel = Me.Controls("Label" & potential)
         compLabel.Text = "X"
+
+        'Reinforcement Learning
         If Not CheckWin() Then
             ComputerTurn()
         End If
     End Sub
 
-    Private Function CheckWin() As Boolean
-        Dim win As Short = 0
+    Private Function Minimax(box() As Integer, branch As Short, deep As Short, is_player_turn As Boolean) As Short
+        Dim reward As Short = GetReward(box)
+        'end recursive loop if got winner, or reached maximum deep
+        If reward <> 0 Or deep = 0 Then
+            Return reward
+        End If
 
-        For j = 1 To 2
-            If box(0) = j And box(4) = j And box(8) = j Then
-                win = j
-            ElseIf box(2) = j And box(4) = j And box(6) = j Then
-                win = j
-            Else
-                For i As Integer = 0 To 2
-                    If box(i) = j And box(i + 3) = j And box(i + 6) = j Then
-                        win = j
-                    ElseIf box(3 * i) = j And box(3 * i + 1) = j And box(3 * i + 2) = j Then
-                        win = j
-                    End If
-                Next
+        Dim rewards As New List(Of Short)
+        Dim max_reward_path As New Dictionary(Of Short, Short)
+
+        Dim number As Short = RND.Next(1, 9)
+
+        For i = 0 To Len(branch)
+            'copy array ByVal
+            Dim tempbox(8) As Integer
+            Array.Copy(box, tempbox, tempbox.Length)
+
+            number = GetRnd(number, tempbox)
+
+            'is_player_turn: true = -1, false = 0
+            tempbox(number) = is_player_turn + 2
+
+            'continue running recursive function, and switch turn
+            rewards.Add(Minimax(tempbox, branch, deep - 1, Not is_player_turn))
+
+            If Not max_reward_path.Keys.Contains(number) Then
+                max_reward_path.Add(number, rewards(i))
             End If
         Next
 
-        If win = 0 And Array.IndexOf(box, 0) = -1 Then
-            win = 3
+        'if finished searching, return the value of position of the highest reward 
+        If deep = Me.deep Then
+            If is_player_turn Then
+                Return max_reward_path.FirstOrDefault(Function(n) n.Value = max_reward_path.Values.Min()).Key
+            Else
+                Return max_reward_path.FirstOrDefault(Function(n) n.Value = max_reward_path.Values.Max()).Key
+            End If
         End If
 
-        If win = 0 Then
-            Return False
-        ElseIf win = 1 Then
-            'b = MsgBox("You win!", MsgBoxStyle.OkOnly, "Congratulation!")
-        ElseIf win = 2 Then
-            'b = MsgBox("Try again later.", MsgBoxStyle.OkOnly, "You lose!")
-        ElseIf win = 3 Then
-            'b = MsgBox("It's a draw", MsgBoxStyle.OkOnly, "Draw")
+        'apply minimax theory
+        If is_player_turn Then
+            Return rewards.Min
+        Else
+            Return rewards.Max
         End If
 
-        If statistic(0).Contains(Turn & movement) Then
-            For i = 0 To statistic(0).Count() - 1
-                If statistic(0)(i).Equals(Turn & movement) Then
-                    Dim value = Integer.Parse(statistic(1)(i)) + win - 2
-                    statistic(1)(i) = value.ToString
+    End Function
+
+    Private Function GetRnd(number As Short, box() As Integer) As Short
+        For i = 0 To 8
+            'pick a random label, choose another one if the label is filled
+            number = ((number + 1) Mod 9)
+            If box(number) = 0 Then
+                Return number
+            End If
+        Next
+    End Function
+
+    Private Function GetWinner(box() As Integer) As Short
+        Dim winner As Short = -1
+        'check diagonal win status
+        If (box(0) = box(4) And box(4) = box(8)) Or (box(2) = box(4) And box(4) = box(6)) Then
+            winner = box(4)
+        Else
+            For i As Integer = 0 To 2
+                'check column and row win status
+                If box(i) = box(i + 3) And box(i + 3) = box(i + 6) Then
+                    winner = box(i)
+                ElseIf box(3 * i) = box(3 * i + 1) And box(3 * i + 1) = box(3 * i + 2) Then
+                    winner = box(3 * i)
                 End If
             Next
-        Else
-            statistic(0).Add(Turn & movement)
-            statistic(1).Add(win - 2)
+        End If
+        Return winner
+    End Function
+
+    Private Function GetReward(box() As Integer) As Short
+        Dim winner As Short = GetWinner(box)
+        Dim reward As Short = 0
+
+        If winner = 1 Then
+            'player wins
+            reward = -1
+            'b = MsgBox("You win!", MsgBoxStyle.OkOnly, "Congratulation!")
+        ElseIf winner = 2 Then
+            'computer wins
+            reward = 2
+            'b = MsgBox("Try again later.", MsgBoxStyle.OkOnly, "You lose!")
+        ElseIf winner = -1 And Array.IndexOf(box, 0) = -1 Then
+            'check if no winner and all label is filled
+            reward = 1
+            'b = MsgBox("It's a draw!", MsgBoxStyle.OkOnly, "Draw!")
         End If
 
-        If statistic.Count > 0 Then
-            Dim tempStatistic As New List(Of String)
-            For i = 0 To statistic(0).Count - 1
-                tempStatistic.Add(statistic(0)(i) + "," + statistic(1)(i))
+        Return reward
+
+    End Function
+
+    Private Function CheckWin() As Boolean
+        'set reward for training computer
+        Dim reward As Short = GetReward(box)
+
+        If reward = 0 Then
+            'continue playing
+            Return False
+        End If
+
+        'game ended, store record to database
+        If Not stat_key.Equals("") Then
+            'update database if record exist
+            statistics(stat_key) += reward
+        ElseIf statistics.ContainsKey(Turn & movement) Then
+            'update database if record exist
+            statistics(Turn & movement) += reward
+        Else
+            'otherwise add new record to database
+            statistics.Add(Turn & movement, reward)
+        End If
+
+        'store database into file
+        If statistics.Count > 0 Then
+            Dim tempstatistics As New List(Of String)
+            For Each statistic In statistics
+                tempstatistics.Add(statistic.Key + "," + statistic.Value.ToString)
             Next
-            File.WriteAllLines(path, tempStatistic.ToArray())
+            File.WriteAllLines(path, tempstatistics.ToArray())
         End If
         Static a As Integer = 0
         'a += 1
@@ -199,17 +287,20 @@ end_of_for:
         Next
         box = {0, 0, 0, 0, 0, 0, 0, 0, 0}
         movement = ""
-        statistic.Clear()
-        statistic.Add(New List(Of String))
-        statistic.Add(New List(Of String))
+        stat_key = ""
+        statistics.Clear()
 
-        Dim tempStatistic As New List(Of String)
-        tempStatistic.AddRange(File.ReadAllLines(path))
-        For Each i In tempStatistic
+        'retrive previous playing records from database
+        Dim tempstatistics As New List(Of String)
+        tempstatistics.AddRange(File.ReadAllLines(path))
+        For Each i In tempstatistics
+            'store playing movements and points into statistics
+            'statistics Key: movements; Value: reward
             Dim s As String() = i.Split(",")
-            statistic(0).Add(s(0))
-            statistic(1).Add(s(1))
+            statistics.Add(s(0), s(1))
         Next
+
+        'check whos turn before start game
         If Turn = 2 Then
             ComputerTurn()
         Else
@@ -218,6 +309,7 @@ end_of_for:
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'create database if databases not exist
         If Not File.Exists(path) Then
             File.WriteAllText(path, "")
         End If
